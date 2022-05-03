28d6e7652f923b9149b57afa61162f95703d47aa
CamillaSimicich <100423042+CamillaSimicich@users.noreply.github.com>
2022-05-03 13:52:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,21 +5,22 @@ using namespace std;
 
 int main(){
   char c;
-  cin >>c;
-  int n = (int)c;
-  bool x = funzione (n);
+  cin >> c;
+  int n = (int) c;
+  bool x = funzione(n);
   
   if (x==0){
     cout << "errore" << endl;
   }else if (x==1){
     if ((65<=n)&&(n<=90)){
       n=n+32;
-      cout <<(char)n << endl;
-    }else if ((97<=n)&&(n<=122)){
+      cout <<(char)n<<endl;
+    } else if ((97<=n)&&(n<=122)){
       n=n-32;
-      cout <<(char)n<< endl;
+      cout<<(char)n<< endl;
     }
   }
+  
   
   return 0;
 }
