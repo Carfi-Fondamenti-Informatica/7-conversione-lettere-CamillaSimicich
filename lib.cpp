ee978680b6317d270b733938ef4bee3ab5926b72
CamillaSimicich <100423042+CamillaSimicich@users.noreply.github.com>
2022-05-03 13:40:03 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,8 @@
 #include "lib.h"
 
+
 bool funzione (int n){
-  if (((65<=n)&&(n<90))|| ((97<=n) && (n<=122))){
+  if (((65<=n)&&(n<90)) || ((97<=n) && (n<=122))){
     return true;
   }else{
     return false;
